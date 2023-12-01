9a53cc3294eca827ff4eeaefd124e7a12eb9089e
nkserveren26 <norinoritoshi@outlook.jp>
2023-12-01 23:59:39 +0900

    commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,38 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+html,
+body,
+textarea {
+  padding: 0;
+  margin: 0;
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, 'Open Sans', 'Helvetica Neue', sans-serif, 
+}
+
+* {
+  box-sizing: border-box;
+}
+
+
+a {
+  cursor: pointer;
+  text-decoration: none;
+  transition: .25s;
+  color: #000
+}
+
+ol, ul {
+  list-style: none;
+}
+`;
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return 
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
 }
