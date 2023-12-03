9996f0fa286527d5db70c400bb3ab8bf99826cfe
nkserveren26 <norinoritoshi@outlook.jp>
2023-12-03 23:09:27 +0900

    correct files

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import { createGlobalStyle } from 'styled-components'
+import "@/styles/globals.css"
+import type { AppProps } from "next/app"
+import { createGlobalStyle } from "styled-components"
 
 const GlobalStyle = createGlobalStyle`
 html,
@@ -26,13 +26,12 @@ a {
 ol, ul {
   list-style: none;
 }
-`;
-
+`
 
 export default function App({ Component, pageProps }: AppProps) {
-  return 
-    <>
-      <GlobalStyle />
-      <Component {...pageProps} />
-    </>
+  return
+  ;<>
+    <GlobalStyle />
+    <Component {...pageProps} />
+  </>
 }
